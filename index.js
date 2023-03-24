c36ff63c26ab4d71316bde0e80c80314f6e83efd
Truc Nguyen <truc.nguyenht@gmail.com>
2023-03-25 02:29:51 +0700

    bai2,3,4

--- index.js
+++ index.js
@@ -13,3 +13,56 @@ document.getElementById("btnKetQuaBai1").onclick = function () {
   Kết quả là ${n - 1}
   `;
 };
+
+//
+// Bài 2: Viết chương trình nhập vào 2 số x, n tính tổng: S(n) = x + x^2 + x^3 + … + x^n (Sử dụng vòng lặp và hàm)
+// =====================================================
+function tinhKetQuaBai2(x, n) {
+  var sum = 0;
+  for (var i = 1; i <= n; i++) {
+    sum += Math.pow(x, i);
+  }
+  return sum;
+}
+
+document.getElementById("btnKetQuaBai2").onclick = function () {
+  var x = document.getElementById("so-x").value * 1;
+  var n = document.getElementById("so-n").value * 1;
+  var sum = tinhKetQuaBai2(x, n);
+
+  document.getElementById("ketquabai2").innerHTML = `
+  Kết Quả: ${sum}
+  `;
+};
+
+//
+// Bài 3: Nhập vào n. Tính giai thừa 1*2*...n
+// =====================================================
+function tinhGiaiThua(n) {
+  if (n === 0 || n === 1) {
+    return 1;
+  } else {
+    return n * tinhGiaiThua(n - 1);
+  }
+}
+
+document.getElementById("btnKetQuaBai3").onclick = function () {
+  var n = document.getElementById("so-m").value * 1;
+  var giaiThua = tinhGiaiThua(n);
+
+  document.getElementById("ketQuaBai3").innerHTML = `
+  Kết quả: ${giaiThua}
+  `;
+};
+
+//
+// Bài 4: Nhập vào n. Tính giai thừa 1*2*...n
+// =====================================================
+document.getElementById("addNewDiv").onclick = function taoDiv() {
+  for (var i = 1; i <= 10; i++) {
+    var div = document.createElement("div");
+    div.innerText = i;
+    div.className = i % 2 === 0 ? "red" : "green";
+    document.body.appendChild(div);
+  }
+};
